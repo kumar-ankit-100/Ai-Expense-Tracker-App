094da305bf0f389f20870cae434561d2afce4ef2
Ankit kumar <kumar.ankitr0321@gmail.com>
2026-02-10 00:14:08 +0530

    Simplify babel config and remove asset paths

--- babel.config.js
+++ babel.config.js
@@ -4,25 +4,6 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '@': './src',
-            '@components': './src/components',
-            '@screens': './src/screens',
-            '@navigation': './src/navigation',
-            '@store': './src/store',
-            '@services': './src/services',
-            '@utils': './src/utils',
-            '@hooks': './src/hooks',
-            '@theme': './src/theme',
-            '@constants': './src/constants',
-            '@assets': './src/assets',
-          },
-        },
-      ],
     ],
   };
 };
