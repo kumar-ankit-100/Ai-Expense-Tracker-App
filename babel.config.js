4b0239a791a5fcf0d5fb927d0599fdb50edb17bc
Ankit kumar <kumar.ankitr0321@gmail.com>
2026-02-10 00:18:33 +0530

    Add babel-plugin-module-resolver for path aliases

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,26 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@': './src',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@navigation': './src/navigation',
+            '@store': './src/store',
+            '@services': './src/services',
+            '@utils': './src/utils',
+            '@hooks': './src/hooks',
+            '@theme': './src/theme',
+            '@constants': './src/constants',
+            '@types': './src/types',
+            '@assets': './src/assets',
+          },
+        },
+      ],
       'react-native-reanimated/plugin',
     ],
   };
